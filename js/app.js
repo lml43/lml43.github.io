70688cb2a4b1fdb91650adf38e9b6832f5d6e420
manhlinh0403 <manhlinh0403@gmail.com>
2021-04-01 21:32:48 +0200

    About page

--- js/app.js
+++ js/app.js
@@ -13,17 +13,18 @@ new fullpage('#fullpage', {
 
         // des section
         const des_section = des.item;
-        const des_title = des_section.querySelector('h1');
-        const des_bars = des_section.querySelectorAll('span');
-        const des_description = des_section.querySelector('p');
-        
-        new TimelineMax({delay: 0.5}).fromTo(des_title, 0.4, {x: -500}, {x: 0});
-        des_bars.forEach(bar => {
-            new TimelineMax({delay: 0.7}).fromTo(bar, 0.5, {x: -150}, {x: 0});
-        });
-        new TimelineMax({delay: 0.8}).fromTo(des_description, 0.6, {x: -200}, {x: 0});
-        console.log(des.index)
+        const des_title = des_section.querySelector('.title h1');
+        const des_bars = des_section.querySelectorAll('.border span');
+        const des_description = des_section.querySelector('.slide-in-left');
+
         if (des.index === 0) {
+
+            new TimelineMax({delay: 0.5}).fromTo(des_title, 0.4, {x: -500}, {x: 0});
+            des_bars.forEach(bar => {
+                new TimelineMax({delay: 0.7}).fromTo(bar, 0.5, {x: -150}, {x: 0});
+            });
+            new TimelineMax({delay: 0.8}).fromTo(des_description, 0.6, {x: -200}, {x: 0});
+
             let imgs1 = des_section.querySelectorAll('.section-img-1');
             let imgs2 = des_section.querySelectorAll('.section-img-2');
 
@@ -34,13 +35,26 @@ new fullpage('#fullpage', {
             imgs2.forEach(des_img => {
                 new TimelineMax({delay: 0.3}).fromTo(des_img, 1, {x: 200, opacity: 0}, {x: 0, opacity: 1});
             })
+        } else {
+            new TimelineMax({delay: 0.5}).fromTo(des_title, 0.4, {x: -700}, {x: 0});
+            des_bars.forEach(bar => {
+                new TimelineMax({delay: 0.7}).fromTo(bar, 0.5, {x: -150}, {x: 0});
+            });
+            new TimelineMax({delay: 0.8}).fromTo(des_description, 0.6, {x: -600}, {x: 0});
+        
+            let fills = des_section.querySelectorAll('.fill');
+
+            console.log(fills)
+            fills.forEach(bar => {
+                bar.classList.add('progress');
+                console.log(bar.classList)
+                
+            })
         }
 
-        console.log(org)
         if (org.index === 0) {
 
             const org_section = org.item;
-            console.log("hello")
             let imgs1 = org_section.querySelectorAll('.section-img-1');
             let imgs2 = org_section.querySelectorAll('.section-img-2');
 
@@ -50,8 +64,15 @@ new fullpage('#fullpage', {
 
             imgs2.forEach(orgImg => {
                 new TimelineMax(0.2).fromTo(orgImg, 0.5, {x: 0, opacity: 1}, {x: 200, opacity: 0});
+            })            
+        } else {
+            let fills = org.item.querySelectorAll('.fill');
+
+            console.log(fills)
+            fills.forEach(bar => {
+                bar.classList.remove('progress');
+                console.log(bar.classList)
             })
-            
         }
     }
 });
