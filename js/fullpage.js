93d4760af5454af9aef560d6eb91a6855bc8cd2b
manhlinh0403 <manhlinh0403@gmail.com>
2021-04-09 01:57:57 +0200

    animation on init page

--- js/fullpage.js
+++ js/fullpage.js
@@ -13,23 +13,9 @@ new fullpage('#fullpage', {
         const des_description = des_section.querySelector('.slide-in-left');
 
         if (des.index === 0) {
-
-            new TimelineMax({ delay: 0.5 }).fromTo(des_title, 0.4, { x: -500 }, { x: 0 });
-            des_bars.forEach(bar => {
-                new TimelineMax({ delay: 0.7 }).fromTo(bar, 0.5, { x: -150 }, { x: 0 });
-            });
-            new TimelineMax({ delay: 0.8 }).fromTo(des_description, 0.6, { x: -200 }, { x: 0 });
-
             let imgs1 = des_section.querySelectorAll('.section-img-1');
             let imgs2 = des_section.querySelectorAll('.section-img-2');
-
-            imgs1.forEach(des_img => {
-                new TimelineMax({ delay: 0.5 }).fromTo(des_img, 1, { x: 200, opacity: 0 }, { x: 0, opacity: 1 });
-            })
-
-            imgs2.forEach(des_img => {
-                new TimelineMax({ delay: 0.3 }).fromTo(des_img, 1, { x: 200, opacity: 0 }, { x: 0, opacity: 1 });
-            })
+            animateSection1(des_section, des_title, des_bars, des_description, imgs1, imgs2)
         } else if (des.index === 1) {
             new TimelineMax({ delay: 0.5 }).fromTo(des_title, 0.4, { x: -700 }, { x: 0 });
             des_bars.forEach(bar => {
@@ -68,7 +54,42 @@ new fullpage('#fullpage', {
     }
 });
 
+function animateSection1(section, title, bars, description, imgs1, imgs2) {
+    new TimelineMax({ delay: 0.5 }).fromTo(title, 0.4, { x: -500 }, { x: 0 });
+    bars.forEach(bar => {
+        new TimelineMax({ delay: 0.7 }).fromTo(bar, 0.5, { x: -150 }, { x: 0 });
+    });
+    new TimelineMax({ delay: 0.8 }).fromTo(description, 0.6, { x: -200 }, { x: 0 });
 
+
+
+    imgs1.forEach(des_img => {
+        new TimelineMax({ delay: 0.5 }).fromTo(des_img, 1, { x: 200, opacity: 0 }, { x: 0, opacity: 1 });
+    })
+
+    imgs2.forEach(des_img => {
+        new TimelineMax({ delay: 0.3 }).fromTo(des_img, 1, { x: 200, opacity: 0 }, { x: 0, opacity: 1 });
+    })
+}
+
+// Animation at app begining
+function initAnimation() {
+    // des section
+    const section = $('.s1');
+    const title = $('.s1 .title h1');
+    const bars = $('.s1 .border-line').children().toArray();
+    const description = $('.s1 .slide-in-left');
+
+    let imgs1 = $('.section-img-1').toArray();
+    let imgs2 = $('.section-img-2').toArray();
+
+    console.log(imgs1)
+    animateSection1(section, title, bars, description, imgs1, imgs2);
+}
+
+initAnimation()
+
+// Mixitup
 $('#mix-wrapper').mixItUp({
     load: {
         sort: 'order:asc'
@@ -89,46 +110,7 @@ $('#mix-wrapper').mixItUp({
     }
 });
 
-// CONTACT FORM
-// var form = document.getElementById("contact-form");
-
-// async function handleSubmit(event) {
-//     event.preventDefault();
-//     var status = document.getElementById("contact-form-status");
-//     var data = new FormData(event.target);
-//     fetch(event.target.action, {
-//         method: form.method,
-//         body: data,
-//         headers: {
-//             'Accept': 'application/json'
-//         }
-//     }).then(response => {
-//         status.innerHTML = "Thanks for your submission!";
-//         form.reset()
-//     }).catch(error => {
-//         status.innerHTML = "Oops! There was a problem submitting your form"
-//     });
-// }
-// form.addEventListener("submit", handleSubmit)
-
-
-// CONTACT FORM
-// $('#contact-form').submit(function(e) {
-//     e.preventDefault();
-
-//     $.ajax({
-//         url: 'https://formspree.io/f/xleaewap',
-//         method: 'POST',
-//         data: { message: $('form').serialize() },
-//         dataType: 'json'
-//     }).done(function(response) {
-//         $('#success').addClass('expand');
-//         $('#contact-form')
-//             .find('input[type=text], input[type=email], textarea')
-//             .val('');
-//     });
-// });
-
+// Contact form
 var form = document.getElementById("contact-form");
 
 async function handleSubmit(event) {
